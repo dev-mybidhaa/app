70c3d2f406d834c68b371da5d38510915622870d
Kevah Web Developers <kevahwebsolutions@gmail.com>
2025-05-11 01:28:59 +0300

    cart.js for index, added preloader.js

---
+++ public/js/preloader.js
@@ -0,0 +1,5 @@
+// Hide preloader when page fully loads
+                window.addEventListener("load", function() {
+                    document.getElementById("biof-loading").style.display = "none";
+                    document.getElementById("content").style.display = "block";
+                });
